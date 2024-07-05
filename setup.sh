c09f5c4ff5507cf60ae5c93af4c8dbc5bc84bfbc
Mats Thor <3275180+thors-beard@users.noreply.github.com>
2024-07-05 10:59:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-brew tap homebrew/cask-fonts
-
 brew install \
     starship \
     vim \
